9125702ec0828dfcb6f4c3f68b9d5460030820a9
Vansh Shah <110724108+Vansh1shah@users.noreply.github.com>
2024-10-25 17:37:52 +0530

    Delete main.ts

--- main.ts
+++
@@ -1,9 +0,0 @@
-document.getElementById("login-candidate")?.addEventListener("click", () => {
-    alert("Candidate login clicked!");
-});
-
-document.getElementById("login-admin")?.addEventListener("click", () => {
-    // Redirect to admin-login.html
-    window.location.href = "admin-login.html";
-});
-    
